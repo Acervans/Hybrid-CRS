8f5b3f5bdee81f5b73c39b14535ce6d5602b55fb
Javier Wang Zhou <60742676+Acervans@users.noreply.github.com>
2025-05-19 13:43:14 +0200

    chore: move data_utils tests to separate file

--- hybrid-crs/data_processing/data_utils.py
+++ hybrid-crs/data_processing/data_utils.py
@@ -335,39 +335,3 @@ def process_dataset(
         print("\nCleaning ITEM DataFrame...")
         items_df = clean_dataframe(items_df)
         items_df.to_csv(f"{output_folder}/{dataset_name}.item", sep=SEP, index=False)
-
-
-if __name__ == "__main__":
-    item_headers = get_item_headers(
-        ["iid:token", "movie_title:token_seq", "genre:token", "release_year:float"]
-    )
-    assert item_headers.item_id_column == "iid:token"
-    assert item_headers.name_column == "movie_title:token_seq"
-    assert item_headers.category_column == "genre:token"
-
-    assert (
-        get_item_headers(["iid:token", "movie_title:token_seq"]).category_column == None
-    )
-
-    user_headers = get_user_headers(
-        ["USER_IDENTIFICATION:token", "USER_AGE:float", "USER_NAME:token"]
-    )
-    assert user_headers.user_id_column == "USER_IDENTIFICATION:token"
-
-    inter_headers = get_inter_headers(
-        ["user_id:token", "item_id:token", "rating_value:float", "timestamp:float"]
-    )
-    assert inter_headers.user_id_column == "user_id:token"
-    assert inter_headers.item_id_column == "item_id:token"
-    assert inter_headers.rating_column == "rating_value:float"
-
-    assert get_datatype(["My name is Caesar", "The birth of a mother"]) in (
-        "token",
-        "token_seq",
-    )
-    assert (
-        get_datatype(["Action Comedy", "Adventure", "Drama Animation"]) == "token_seq"
-    )
-    assert get_datatype(["Comedy", "Adventure", "Animation"]) == "token"
-    assert get_datatype([1.12, 2.1, 3.1]) == "float"
-    assert get_datatype(["1 2 3", "2 3 3", "3 1 1"]) in ("float_seq", "token_seq")
